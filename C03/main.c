5ac73ad2cc14b97ff4f19a672c0a819123f9d8b1
Wiatrowiec <krysztoszek@wp.pl>
2018-03-13 09:48:42 +0100

    C03

---
+++ C03/main.c
@@ -0,0 +1,20 @@
+//#include "Z00.txt"
+//#include "Z01.txt" // 3.2.1
+//#include "Z02.txt" // 3.2.2
+//#include "Z03.txt" // 3.2.3
+//#include "Z04.txt" // 3.2.4
+//#include "Z05.txt" // 3.2.5
+//#include "Z06.txt" // 3.2.6
+//#include "Z07.txt" // 3.2.9
+//#include "Z08.txt" // 3.2.10
+//#include "Z09.txt" // 3.2.11
+//#include "Z10.txt" // 3.2.12
+//#include "Z11.txt" // 3.2.13
+#include "Z12.txt" // 3.2.14
+//#include "Z13.txt" // 3.2.15
+//#include "Z14.txt" // 3.2.16
+//#include "Z15.txt"
+//#include "Z16.txt"
+//#include "Z17.txt"
+//#include "Z19.txt"
+//#include "Z20.txt"

611f7b144caf32c6178b021a8f13f5680c28ef9e
Wiatrowiec <krysztoszek@wp.pl>
2018-03-20 12:14:20 +0100

    dd
    dd

---
+++ C04/main.c
@@ -0,0 +1,20 @@
+//#include "Z00.txt"
+//#include "Z01.txt" // 4.2.1
+//#include "Z02.txt" // 4.2.2
+//#include "Z03.txt" // 4.2.3
+//#include "Z04.txt" // 4.2.4
+//#include "Z05.txt" // 4.2.5
+//#include "Z06.txt" // 4.2.6
+//#include "Z07.txt" // 4.2.7
+//#include "Z08.txt" // 4.2.8
+//#include "Z09.txt" // 4.2.9
+//#include "Z10.txt" // 4.2.10
+//#include "Z11.txt" // 4.2.11
+//#include "Z12.txt" // 3.2.12
+//#include "Z14.txt" // 4.2.14
+//#include "Z15.txt" // 4.2.15
+//#include "Z16.txt" // 4.2.16
+//#include "Z17.txt" // 4.2.17
+//#include "Z18.txt" // 4.2.18
+//#include "Z19.txt" // 4.2.19
+//#include "Z20.txt"
